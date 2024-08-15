2e43887144a001fad76348ebb2305de34ac39894
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2024-08-14 20:34:11 -0600

    Update transformations.py

--- transformations.py
+++ transformations.py
@@ -1,3 +1,19 @@
 import pandas as pd
-def get_album_average(tracklist_with_audio_features):
-    # drop live songs
+
+def track_audio_features_to_df(albums_tracks_audio_features_dict):
+    # {'Daemon (Bonus Tracks Version)': {'Aeon Daemonium': {'danceability': 0.309,
+    #                                                       'energy': 0.958,
+    #                                                       'loudness': -8.47,
+    #                                                       'tempo': 156.041,
+    #                                                       'valence': 0.0387},
+    album_audio_features_flattened = {}
+    for album in albums_tracks_audio_features_dict.keys():
+        for track in albums_tracks_audio_features_dict[album].keys():
+            album_audio_features_flattened = {'track_title': track,
+                                                'danceability': albums_tracks_audio_features_dict[album][track]['danceability'],
+                                                'energy': albums_tracks_audio_features_dict[album][track]['energy'],
+                                                'loudness': albums_tracks_audio_features_dict[album][track]['loudness'],
+                                                'tempo': albums_tracks_audio_features_dict[album][track]['tempo'],
+                                                'valence': albums_tracks_audio_features_dict[album][track]['valence'],
+                                              'album': album}
+            print(album_audio_features_flattened)
